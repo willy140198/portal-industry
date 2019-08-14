bc5e7c0e52cfac14ccc5ab955c46e381ed764e9c
Willy Setiawan <taralite@FVFXP1G0J1WK.local>
2019-08-14 17:45:10 +0700

    add login page

--- app/src/main/java/com/example/portal/view/main/MainActivity.kt
+++ app/src/main/java/com/example/portal/view/main/MainActivity.kt
@@ -1,12 +1,13 @@
-package com.example.portal
+package com.example.portal.view.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.portal.R
 
-class LoginActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_main)
     }
 }
